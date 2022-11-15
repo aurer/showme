0a805a12c2206ba21a9159348f45092860ee7e77
Phil Maurer <philmau@gmail.com>
2022-11-15 21:29:21 +0000

    Add referrer and action to params

--- models/param.go
+++ models/param.go
@@ -16,6 +16,8 @@ type Param struct {
 type Params struct {
 	Params []Param
 	CreatedAt string
+	Referer string
+	SubmitPath string
 }
 
 // Hash creates an MD5 hash from a Param slice
@@ -36,4 +38,21 @@ func (p *Params) Add(param Param) {
 
 func (p *Params) Sort() {
 	sort.Slice(p.Params, func(i, j int) bool { return p.Params[i].Name < p.Params[j].Name })	
+}
+
+func (p *Params) GetSubmitPath() string {
+	submitPath := ""
+	index := -1
+	for i, p := range p.Params {
+		if p.Name == "formSubmitsTo" {
+			submitPath = p.Value[0]
+			index = i
+		}
+	}
+
+	if index > -1 {
+		p.Params = append(p.Params[:index], p.Params[index+1:]...)
+	}
+
+	return submitPath
 }
